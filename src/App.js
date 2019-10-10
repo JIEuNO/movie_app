79a55555e3ea28cf27825caa208a99db094165f7
JIEuNo <wisdom990629@naver.com>
2019-10-10 19:45:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie.js';
+
+class App extends Component{ //component -> return -> render
+  state ={
+  }
+
+  componentWillMount(){
+    console.log('will mount')
+  }
+
+  componentDidMount(){
+    this._getMovies()
+  }
+
+  _getMovies = async() => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+   _callApi =() =>{
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err)) // error 처리
+   }
+  _renderMovies =() => {
+    const movies=this.state.movies.map((movie)=>{
+      return <Movie 
+      title={movie.title} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}/>
+  })
+  return movies
+  }
+
+  render(){
+    return(
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    )
+  }
+}
+
+export default App;
